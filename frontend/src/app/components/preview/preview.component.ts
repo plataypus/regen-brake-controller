13b545a3b2250e2dbb7db3caa6fae5ab6757a90f
Jimmy Lam <JimmyLam978@gmail.com>
2021-03-21 13:37:23 -0400

    param update

--- frontend/src/app/components/preview/preview.component.ts
+++ frontend/src/app/components/preview/preview.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit, Input, OnChanges, ChangeDetectorRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { WebsocketService } from '../../services/websocket.service';
 
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.component.html',
-  styleUrls: ['./preview.component.scss']
+  styleUrls: ['./preview.component.scss'],
 })
 export class PreviewComponent implements OnInit, OnChanges {
   // trackLength: number = 1;
@@ -16,20 +22,20 @@ export class PreviewComponent implements OnInit, OnChanges {
     started: false,
     paused: -1,
     ended: false,
-  }
+  };
   graphData: any = {};
   graphSettings: any = {
     maxY: 1,
-  }
-  constructor(private wsService: WebsocketService) { }
+  };
+  constructor(private wsService: WebsocketService) {}
 
   ngOnInit(): void {
-    this.wsService.stateUpdate.subscribe(state => {
-      if (state){
+    this.wsService.stateUpdate.subscribe((state) => {
+      if (state) {
         // this.state = state;
       }
-    })
-    this.computeGraphData()
+    });
+    this.computeGraphData();
   }
 
   ngOnChanges(): void {
@@ -37,67 +43,66 @@ export class PreviewComponent implements OnInit, OnChanges {
   }
 
   computeGraphData(): void {
-    const finalD = Math.pow(this.brakeSpeed, 2) / (2*this.acceleration)
-    const arr = Array.from({length: 11}, (_, index) => index);
+    const finalD = Math.pow(this.brakeSpeed, 2) / (2 * this.acceleration);
+    const arr = Array.from({ length: 11 }, (_, index) => index);
     const data = arr.map((num) => {
-      const x = finalD / 10 * num
-      const y = Math.pow((2*this.acceleration*x), 0.5)
-      return {x, y}
+      const x = (finalD / 10) * num;
+      const y = Math.pow(2 * this.acceleration * x, 0.5);
+      return { x, y };
     });
-    const finalY = Math.pow((2*this.acceleration*finalD), 0.5)
-    data.push({x: 50, y: finalY});
-    data.push({x: 100, y:0});
-    this.graphData = data
+    const finalY = Math.pow(2 * this.acceleration * finalD, 0.5);
+    data.push({ x: 50, y: finalY });
+    data.push({ x: 100, y: 0 });
+    this.graphData = data;
 
     // this.graphSettings = {maxY: finalY + 1}
     // console.log(this.graphData)
   }
 
-
   onStart(): void {
     this.wsService.control({
-      command: "start",
-      params: {
-        brakeTorque: this.brakeTorque, 
+      command: 'start',
+      param: {
+        brakeTorque: this.brakeTorque,
         brakeSpeed: this.brakeSpeed,
         acceleration: this.acceleration,
-      }
-    })
+      },
+    });
 
     this.state = {
       started: true,
       paused: 0,
       ended: false,
-    }
+    };
   }
   onPause(): void {
     this.wsService.control({
-      command: "pause",
-    })
+      command: 'pause',
+    });
 
     this.state = {
       started: true,
-      paused: this.state.paused === 0 ? 1: 0,
+      paused: this.state.paused === 0 ? 1 : 0,
       ended: false,
-    }
+    };
   }
 
   onReset(): void {
     this.wsService.control({
-      command: "reset",
-    })
+      command: 'reset',
+    });
 
     this.state = {
       started: false,
       paused: -1,
       ended: false,
-    }
+    };
   }
 
   speedChange(): void {
-    const newMin = (Math.floor(Math.pow(this.brakeSpeed, 2) / 50) + 1) * 0.5; 
+    const newMin = (Math.floor(Math.pow(this.brakeSpeed, 2) / 50) + 1) * 0.5;
     this.acceleration = newMin;
     this.minAcceleration = newMin;
-    this.computeGraphData()
+    this.computeGraphData();
   }
 }
